dc95614f26faa093501b8714a632e888095a42a9
zoha rehan <64752194+zoharehan@users.noreply.github.com>
2023-07-27 18:07:09 -0400

    project4 challenges completed.

--- project4/BetterRest/BetterRest/ContentView.swift
+++ project4/BetterRest/BetterRest/ContentView.swift
@@ -34,42 +34,83 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    private var cups = 1...20
+    
+    //    Challenge 3: we create a computed property in order to always display the recommended sleep time
+    var computedBedtime: String {
+        var message: String
+            do {
+                let config = MLModelConfiguration()
+                let model = try SleepCalculator(configuration: config)
+                
+    //            separate hours and minutes from the date type -> convert to Double.
+                let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+                let hour = (components.hour ?? 0) * 60 * 60
+                let minute = (components.minute ?? 0) * 60
+                
+                let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+                
+    //            to give us the actual time when the person should sleep.
+                let sleepTime = wakeUp - prediction.actualSleep
+
+                message = sleepTime.formatted(date: .omitted, time: .shortened)
+            } catch {
+                message = "Sorry, there was a problem calculating your bedtime."
+            }
+            return message
+        }
+    
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    
+                Section {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
+                        .labelsHidden().datePickerStyle(WheelDatePickerStyle())
+                } header: {
+                    Text("When do you want to wake up?")
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    
+                Section {
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                } header: {
+                    Text("Desired amount of sleep")
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    
+                Section {
+//  Replace the “Number of cups” stepper with a Picker showing the same range of values.
+//                    Picker(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", selection: $coffeeAmount) {
+//                            ForEach(cups, id: \.self) { cup in
+//                                Text("\(cup)").tag(cup)
+//
+//                            }
+//                    }
                     Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                } header: {
+                    Text("Daily coffee intake")
                 }
+                
+                
+                Section {
+                    Text("\(computedBedtime)").font(.title)
+                    
+                } header: {
+                    Text("Recommended Bedtime")
+                }
+                
             }.navigationTitle("BetterRest")
-                .toolbar {
-                    Button("Calculate", action: calculateBedtime)
-//                        adding an alert to the Form which shows the predicted bedtime.
-                        .alert(alertTitle, isPresented: $showingAlert) {
-                        Button("OK") { }
-                    } message: {
-                        Text(alertMessage)
-                    }
-        }
+//  Challenge 3: Removing the compute button since the bedtime is already displayed.
+//                .toolbar {
+//                    Button("Calculate", action: calculateBedtime)
+//                        .alert(alertTitle, isPresented: $showingAlert) {
+//                        Button("OK") { }
+//                    } message: {
+//                        Text(alertMessage)
+//                    }
+//        }
             }
     }
+    
+    
     func calculateBedtime() {
         do {
             let config = MLModelConfiguration()
